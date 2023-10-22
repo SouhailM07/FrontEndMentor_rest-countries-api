3470e930366027087fc53814f77948ae4194de02
shadow <lightshadow416@gmail.com>
2023-10-22 19:41:50 +0100

    [CountriesContainer] component was created

---
+++ src/components/CountriesContainer/CountriesContainer.tsx
@@ -0,0 +1,4 @@
+import "./countriescontainer.css";
+export default function CountriesContainer() {
+  return <>CountriesContainer</>;
+}
--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { default as MyContainer } from "./MyContainer/MyContainer";
 export { default as Navbar } from "./Navbar/Navbar";
 export { default as SearchBar } from "./SearchBar/SearchBar";
+export { default as CountriesContainer } from "./CountriesContainer/CountriesContainer";
