9806aad3a61854bd0f188a0c93965483eef61389
shadow <lightshadow416@gmail.com>
2023-10-22 18:53:13 +0100

    [Navbar] component checkpoint {1}

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,33 @@
+import React from "react";
+//
 import "./style/input.css";
 import "./App.css";
 // components
 import { MyContainer } from "./components";
+// hooks
+import { useState } from "react";
+
+//@ts-ignore
+export const ThemeContext = React.createContext();
+
 function App() {
+  let body = document.body as HTMLBodyElement;
+  //
+  let [mode, setMode] = useState("light");
+  let switchMode = () => {
+    if (mode == "light") setMode("dark");
+    else if (mode == "dark") setMode("light");
+    console.log(mode);
+  };
+  //
+  body.id = `body--${mode}`;
   return (
     <>
-      <div>
-        <MyContainer />
-      </div>
+      <ThemeContext.Provider value={{ mode, switchMode }}>
+        <div>
+          <MyContainer />
+        </div>
+      </ThemeContext.Provider>
     </>
   );
 }
