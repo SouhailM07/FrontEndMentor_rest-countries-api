aed7054be056aac7731042890fc7b047d954676e
shadow <lightshadow416@gmail.com>
2023-10-22 19:48:39 +0100

    [Country] component was created

---
+++ src/components/Country/Country.tsx
@@ -0,0 +1,4 @@
+import "./country.css";
+export default function Country() {
+  return <>Country</>;
+}
--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@ export { default as MyContainer } from "./MyContainer/MyContainer";
 export { default as Navbar } from "./Navbar/Navbar";
 export { default as SearchBar } from "./SearchBar/SearchBar";
 export { default as CountriesContainer } from "./CountriesContainer/CountriesContainer";
+export { default as Country } from "./Country/Country";
